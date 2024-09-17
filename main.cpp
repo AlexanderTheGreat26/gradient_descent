e0c8f51b43b78b67ef2ed8019f7748be13b32138
AlexanderTheGreat26 <kaegod@yandex.ru>
2024-09-17 22:35:19 +0300

    Works, but need refactoring.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iostream>
 #include <random>
 #include <vector>
 #include <utility>
@@ -16,13 +15,21 @@ const double x_max = 4.0;
 const double y_min = -3.0;
 const double y_max = 4.0;
 const double step = 0.01;
-
+const double eps = std::numeric_limits<double>::epsilon();
+const double l = 0.1;
 
 typedef std::vector<double> grid1;
+//typedef std::array<double, 3> coordinates;
+//typedef std::array<double, 2> vector_coordinates;
+//typedef std::vector<coordinates> grid3;
+
 typedef std::tuple<double, double, double> coordinates;
 typedef std::tuple<double, double> vector_coordinates;
 typedef std::vector<coordinates> grid3;
 
+
+std::random_device rd;  // Will be used to obtain a seed for the random number engine.
+std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd().
 
 
 grid1 mesh (double left_border, const double & right_border, const double & mesh_step);
@@ -36,12 +43,25 @@ void file_creation (const std::string & file_name, std::vector<Tp...> & data);
 
 void plot (const std::string & name, const std::string & title);
 
+grid3 gradient_descent (coordinates q_start, coordinates neighbour_point, double f(double & x, double & y));
+
+
+
+
 int main() {
     grid1 x_grid = std::move(mesh(x_min, x_max, step));
     grid1 y_grid = std::move(mesh(y_min, y_max, step));
     grid3 McCormick = std::move(function_nodes(x_grid, y_grid, kenny));
-    file_creation ("kenny_test", McCormick);
-    plot("kenny_test", "McCormick function");
+//    file_creation ("kenny_test", McCormick);
+//    plot("kenny_test", "McCormick function");
+
+    std::uniform_int_distribution<> dis_node (1, McCormick.size()-2);
+    int start_node = 0; // = dis_node(gen);
+
+    grid3 traj = gradient_descent(McCormick[start_node], McCormick[start_node+750], kenny);
+    file_creation ("kenny_traj", traj);
+
+
     return 0;
 }
 
@@ -50,6 +70,7 @@ int main() {
 
 double kenny (double & x, double & y) {
     return std::sin(x + y) + std::pow(x - y, 2.0) - 1.5*x + 2.5*y + 1.0;
+    //return std::pow(x, 2) /10.0 + std::pow(y, 2);
 }
 
 
@@ -62,52 +83,71 @@ grid3 function_nodes (grid1 & x_grid, grid1 & y_grid, double f(double & x, doubl
 }
 
 
-bool is_equal (double & a, double b) {
-    return std::fabs(a - b) < std::numeric_limits<double>::epsilon();
+double diff (double & f_curr, double & f_prev, double dx) {
+    if (std::fabs(dx) < std::numeric_limits<double>::epsilon())
+        return 0;
+    else
+        return (f_curr - f_prev) / dx;
 }
 
 
-double diff (double & f_prev, double & f_next, const double & q_step) {
-    return (f_next - f_prev) / (2.0 * q_step);
-}
-
-// It should be rewrited is template if it would be usable as recursion of use array instead
-void find_neighbours (grid3 & nodes, int & node, std::tuple<double, double> & x_neighbours, std::tuple<double, double> y_neighbours) {
-    int y_left, y_rigth, x_left, x_right;
-    //std::get<0>
-    for (int i = node; i >= 0; --i)
-        if (!is_equal(std::get<1>(nodes[i]), std::get<1>(nodes[node])) && is_equal(std::get<0>(nodes[i]), std::get<0>(nodes[node]))) {
-            y_left = i;
-            break;
-        }
-    for (int i = node; i < nodes.size(); ++i)
-        if (!is_equal(std::get<1>(nodes[i]), std::get<1>(nodes[node])) && is_equal(std::get<0>(nodes[i]), std::get<0>(nodes[node]))) {
-            y_rigth = i;
-            break;
-        }
-    y_neighbours = std::make_tuple(std::get<2>(nodes[y_left]), std::get<2>(nodes[y_rigth]));
-    //std::get<1>
-    for (int i = node; i >= 0; --i)
-        if (is_equal(std::get<1>(nodes[i]), std::get<1>(nodes[node])) && !is_equal(std::get<0>(nodes[i]), std::get<0>(nodes[node]))) {
-            x_left = i;
-            break;
-        }
-    for (int i = node; i < nodes.size(); ++i)
-        if (is_equal(std::get<1>(nodes[i]), std::get<1>(nodes[node])) && !is_equal(std::get<0>(nodes[i]), std::get<0>(nodes[node]))) {
-            x_right = i;
-            break;
-        }
-    x_neighbours = std::make_tuple(std::get<2>(nodes[x_left]), std::get<2>(nodes[x_right]));
+vector_coordinates grad (vector_coordinates & current_point, vector_coordinates & new_point, double f (double & x, double & y)) {
+    double x_curr = std::get<0>(current_point);
+    double y_curr = std::get<1>(current_point);
+    double x_new = std::get<0>(new_point);
+    double y_new = std::get<1>(new_point);
+    double dx = x_new - x_curr;
+    double dy = y_new - y_curr;
+    double f_curr = f(x_curr, y_curr);
+    double f_x_new = f(x_new, y_curr);
+    double f_y_new = f(x_curr, y_new);
+    return std::make_tuple(diff(f_x_new, f_curr, dx), diff(f_y_new, f_curr, dy));
 }
 
 
-vector_coordinates grad (grid3 & nodes, int & node) {
-    std::tuple<double, double> x_neighbours, y_neighbours;
-    find_neighbours(nodes, node, x_neighbours, y_neighbours);
-    double dfdx = //create template
-    double dfdy = //!!
-    return std::make_tuple
+template<size_t Is = 0, typename... Tp>
+void new_point (std::tuple<Tp...> & point, std::tuple<Tp...> & function_gradient, const double & lambda) {
+        std::get<Is>(point) -= lambda * std::get<Is>(function_gradient);
+    if constexpr (Is + 1 != sizeof...(Tp))
+        new_point<Is + 1>(point, function_gradient, lambda);
 }
+
+
+//template<typename T>
+grid3 gradient_descent (coordinates q_start, coordinates neighbour_point, double f(double & x, double & y)) {
+    // We need to points to have numerical gradient. Let's take some random point and it's neighbour for grad calc.
+    double old_value, new_value = std::get<2>(q_start);
+    vector_coordinates current_point = std::make_tuple(std::get<0>(q_start), std::get<1>(q_start));
+    vector_coordinates next_point = std::make_tuple(std::get<0>(neighbour_point), std::get<1>(neighbour_point));
+    vector_coordinates grad_value = grad(current_point, next_point, f);
+
+    grid3 trajectory {q_start, neighbour_point};
+
+
+
+    do {
+        int n = trajectory.size()-1; // length of trajectory.
+
+        old_value = new_value;
+
+        current_point = std::make_tuple(std::get<0>(trajectory[n]), std::get<1>(trajectory[n]));
+
+        next_point = current_point;
+
+        new_point(next_point, grad_value, l);
+
+        grad_value = grad(current_point, next_point, f);
+
+        new_value = f(std::get<0>(next_point), std::get<1>(next_point));
+
+        trajectory.emplace_back(std::make_tuple(std::get<0>(next_point), std::get<1>(next_point), new_value));
+
+    } while (std::fabs(old_value - new_value) > eps);
+    return trajectory;
+}
+
+
+
 
 
 grid1 mesh (double left_border, const double & right_border, const double & mesh_step) {
@@ -148,6 +188,21 @@ void file_creation (const std::string & file_name, std::vector<Tp...> & data) {
 }
 
 
+
+//template<typename... Tp>
+//void file_creation (const std::string & file_name, grid3 & data) {
+//    std::ofstream fout;
+//    fout.open(file_name, std::ios::trunc);
+//    int N = data[0].size();
+//    for (auto & i : data) {
+//        for (int j = 0; j < N; ++j)
+//            fout << i[j] << '\t';
+//        fout << '\n';
+//    }
+//    fout.close();
+//}
+
+
 void plot (const std::string & name, const std::string & title) {
     FILE *gp = popen("gnuplot -persist", "w");
     if (!gp) throw std::runtime_error("Error opening pipe to GNUplot.");
@@ -173,3 +228,28 @@ void plot (const std::string & name, const std::string & title) {
         fprintf(gp, "%s\n", it.c_str());
     pclose(gp);
 }
+
+//int period (const double & q_min, const double & q_max, const double & mesh_step) {
+//    return (q_max - q_min) / mesh_step;
+//}
+
+
+//std::array<std::array<double,2>, 2> neighbours (grid3 & nodes, int & node, const double & mesh_step) {
+//    int x_period = period(nodes[0][0], nodes[nodes.size()-1][0], mesh_step);
+//    int y_period = period(nodes[0][1], nodes[nodes.size()-1][1], mesh_step);
+//    std::array<double,2> x_neighbours = {nodes[2][node - y_period], nodes[node + y_period][2]};
+//    std::array<double,2> y_neighbours = {nodes[2][node - x_period], nodes[node + x_period][2]};
+//    return {x_neighbours, y_neighbours};
+//}
+//
+//
+//vector_coordinates grad (grid3 & nodes, int & node, const double & mesh_step) {
+//    auto q_neighbours = neighbours(nodes, node, mesh_step);
+//    double f_x_prev = q_neighbours[0][0];
+//    double f_x_next = q_neighbours[0][1];
+//    double f_y_prev = q_neighbours[1][0];
+//    double f_y_next = q_neighbours[1][1];
+//    double dfdx = diff(f_x_prev, f_x_next, mesh_step);
+//    double dfdy = diff(f_y_prev, f_y_next, mesh_step);
+//    return {dfdx, dfdy};
+//}
