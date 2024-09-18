995743bcdeef39cbdaf3de335bd8441c0c522bbb
AlexanderTheGreat26 <kaegod@yandex.ru>
2024-09-18 22:38:05 +0300

    Works, but need refactoring...

--- main.cpp
+++ main.cpp
@@ -19,10 +19,6 @@ const double eps = std::numeric_limits<double>::epsilon();
 const double l = 0.1;
 
 typedef std::vector<double> grid1;
-//typedef std::array<double, 3> coordinates;
-//typedef std::array<double, 2> vector_coordinates;
-//typedef std::vector<coordinates> grid3;
-
 typedef std::tuple<double, double, double> coordinates;
 typedef std::tuple<double, double> vector_coordinates;
 typedef std::vector<coordinates> grid3;
@@ -43,7 +39,7 @@ void file_creation (const std::string & file_name, std::vector<Tp...> & data);
 
 void plot (const std::string & name, const std::string & title);
 
-grid3 gradient_descent (coordinates q_start, coordinates neighbour_point, double f(double & x, double & y));
+grid3 gradient_descent (coordinates q_1st, coordinates q_2nd, double f(double & x, double & y));
 
 
 
@@ -52,25 +48,19 @@ int main() {
     grid1 x_grid = std::move(mesh(x_min, x_max, step));
     grid1 y_grid = std::move(mesh(y_min, y_max, step));
     grid3 McCormick = std::move(function_nodes(x_grid, y_grid, kenny));
-//    file_creation ("kenny_test", McCormick);
-//    plot("kenny_test", "McCormick function");
-
+    file_creation ("function", McCormick);
     std::uniform_int_distribution<> dis_node (1, McCormick.size()-2);
-    int start_node = 0; // = dis_node(gen);
-
-    grid3 traj = gradient_descent(McCormick[start_node], McCormick[start_node+750], kenny);
-    file_creation ("kenny_traj", traj);
-
-
+    int i = dis_node(gen);
+    int j = dis_node(gen);
+    grid3 traj = gradient_descent(McCormick[i], McCormick[j], kenny);
+    file_creation ("trajectory", traj);
+    //plot("kenny_test", "McCormick function");
     return 0;
 }
 
 
-
-
 double kenny (double & x, double & y) {
     return std::sin(x + y) + std::pow(x - y, 2.0) - 1.5*x + 2.5*y + 1.0;
-    //return std::pow(x, 2) /10.0 + std::pow(y, 2);
 }
 
 
@@ -113,41 +103,24 @@ void new_point (std::tuple<Tp...> & point, std::tuple<Tp...> & function_gradient
 }
 
 
-//template<typename T>
-grid3 gradient_descent (coordinates q_start, coordinates neighbour_point, double f(double & x, double & y)) {
-    // We need to points to have numerical gradient. Let's take some random point and it's neighbour for grad calc.
-    double old_value, new_value = std::get<2>(q_start);
-    vector_coordinates current_point = std::make_tuple(std::get<0>(q_start), std::get<1>(q_start));
-    vector_coordinates next_point = std::make_tuple(std::get<0>(neighbour_point), std::get<1>(neighbour_point));
+grid3 gradient_descent (coordinates q_1st, coordinates q_2nd, double f(double & x, double & y)) {
+    double old_value, new_value = std::get<2>(q_1st);
+    vector_coordinates current_point = std::make_tuple(std::get<0>(q_1st), std::get<1>(q_1st));
+    vector_coordinates next_point = std::make_tuple(std::get<0>(q_2nd), std::get<1>(q_2nd));
     vector_coordinates grad_value = grad(current_point, next_point, f);
-
-    grid3 trajectory {q_start, neighbour_point};
-
-
-
+    grid3 trajectory {q_1st, q_2nd};
     do {
-        int n = trajectory.size()-1; // length of trajectory.
-
+        int n = trajectory.size()-1;
         old_value = new_value;
-
         current_point = std::make_tuple(std::get<0>(trajectory[n]), std::get<1>(trajectory[n]));
-
         next_point = current_point;
-
         new_point(next_point, grad_value, l);
-
         grad_value = grad(current_point, next_point, f);
-
         new_value = f(std::get<0>(next_point), std::get<1>(next_point));
-
         trajectory.emplace_back(std::make_tuple(std::get<0>(next_point), std::get<1>(next_point), new_value));
-
     } while (std::fabs(old_value - new_value) > eps);
     return trajectory;
 }
-
-
-
 
 
 grid1 mesh (double left_border, const double & right_border, const double & mesh_step) {
@@ -181,26 +154,11 @@ std::string tuple2string (const Tuple& t) {
 template<typename... Tp>
 void file_creation (const std::string & file_name, std::vector<Tp...> & data) {
     std::ofstream fout;
-    fout.open(file_name, std::ios::trunc);
+    fout.open(file_name+".txt", std::ios::trunc);
     for (auto & i : data)
         fout << tuple2string(i) << '\n';
     fout.close();
 }
-
-
-
-//template<typename... Tp>
-//void file_creation (const std::string & file_name, grid3 & data) {
-//    std::ofstream fout;
-//    fout.open(file_name, std::ios::trunc);
-//    int N = data[0].size();
-//    for (auto & i : data) {
-//        for (int j = 0; j < N; ++j)
-//            fout << i[j] << '\t';
-//        fout << '\n';
-//    }
-//    fout.close();
-//}
 
 
 void plot (const std::string & name, const std::string & title) {
@@ -213,7 +171,6 @@ void plot (const std::string & name, const std::string & title) {
                                       "set autoscale xfix",
                                       "set key off",
                                       "set ticslevel 0",
-                                      //"set border 4095",
                                       "set hidden3d",
                                       "set dgrid3d 50,50 qnorm 4",
                                       "splot \'" + name + ".txt\' w l",
@@ -228,28 +185,3 @@ void plot (const std::string & name, const std::string & title) {
         fprintf(gp, "%s\n", it.c_str());
     pclose(gp);
 }
-
-//int period (const double & q_min, const double & q_max, const double & mesh_step) {
-//    return (q_max - q_min) / mesh_step;
-//}
-
-
-//std::array<std::array<double,2>, 2> neighbours (grid3 & nodes, int & node, const double & mesh_step) {
-//    int x_period = period(nodes[0][0], nodes[nodes.size()-1][0], mesh_step);
-//    int y_period = period(nodes[0][1], nodes[nodes.size()-1][1], mesh_step);
-//    std::array<double,2> x_neighbours = {nodes[2][node - y_period], nodes[node + y_period][2]};
-//    std::array<double,2> y_neighbours = {nodes[2][node - x_period], nodes[node + x_period][2]};
-//    return {x_neighbours, y_neighbours};
-//}
-//
-//
-//vector_coordinates grad (grid3 & nodes, int & node, const double & mesh_step) {
-//    auto q_neighbours = neighbours(nodes, node, mesh_step);
-//    double f_x_prev = q_neighbours[0][0];
-//    double f_x_next = q_neighbours[0][1];
-//    double f_y_prev = q_neighbours[1][0];
-//    double f_y_next = q_neighbours[1][1];
-//    double dfdx = diff(f_x_prev, f_x_next, mesh_step);
-//    double dfdy = diff(f_y_prev, f_y_next, mesh_step);
-//    return {dfdx, dfdy};
-//}
